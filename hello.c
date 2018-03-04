60f03c3daaa35bb9faa9570ed561f213af06c4fb
Sherly17 <spoorthi@iitk.acin>
2018-03-05 02:27:00 +0530

    adding hello.c

--- hello.c
+++ hello.c
@@ -1,12 +1,32 @@
 {\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf830
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
+{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+\deftab720
+\pard\pardeftab720\sl360\sa240\partightenfactor0
 
-\f0\fs36 \cf0 #include<stdio.h>\
-void main()\
+\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
+#include<stdio.h> 
+\fs24 \
+
+\fs32 void microkernel_sendmsg(char *); 
+\fs24 \
+
+\fs32 void main()\
 \{\
- printf("Helloworld!\\n");\
-\}}
+ printf("Helloworld!\\n"); \
+ microkernel_sendmsg("is more portable"); 
+\fs24 \
+
+\fs32 \} 
+\fs24 \
+
+\fs32 void microkernel_sendmsg(char *a)\
+\{ \
+ printf("microkernel: %s\\n", a); 
+\fs24 \
+
+\fs32 \}\uc0\u8232 
+\fs24 \
+}
