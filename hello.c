318ee65345ebd659ad3cbc3a5ef979279aedd587
Sherly17 <spoorthi@iitk.ac.in>
2018-03-05 06:46:37 +0530

    5

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 void main()
-{
- printf("Helloworld!\n"); 
- printf("This must be a monolithic design\n"); 
- microkernel_sendmsg("is more portable");
+{ 
+  printf("Helloworld!\n"); 
+  printf("This must be a monolithic design\n");
+}
+void microkernel_getmsg(char *b)
+{ 
+  //TODO: getmsg feature
 }
